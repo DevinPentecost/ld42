aa165f6aadc3a5f52586911414cb39665b9ed220
Erin Pentecost <erin@pentecost.email>
2018-08-11 00:03:30 -0700

    more bio tweaks

--- project/objects/dogbio.gd
+++ project/objects/dogbio.gd
@@ -33,18 +33,21 @@ func _ready():
 	if ageMonths > 20:
 		ageStr = str(ageMonths / 12) + " year"
 	
-	var adjective = adjectives.AllAdjectives[randi()%(adjectives.AllAdjectives.size())].to_lower()
-	
-	var object = objects.AllObjects[randi()%(objects.AllObjects.size())].to_lower()
-	
 	if sexIndex == 0:
 		Name = _titleCase(names.GirlNames[randi()%(names.GirlNames.size())])
 	else:
 		Name = _titleCase(names.BoyNames[randi()%(names.BoyNames.size())])
 	
+	var adjective = adjectives.AllAdjectives[randi()%(adjectives.AllAdjectives.size())].to_lower()
+	var object = objects.AllObjects[randi()%(objects.AllObjects.size())].to_lower()
 	var swapper = {"name": Name, "girl": sex, "she": she, "her": her, "age": ageStr, "adjective": adjective, "objects": object}
 	
 	var intro = sentences.IntroSentences[randi()%(sentences.IntroSentences.size())].format(swapper)
+	
+	# redo randoms
+	adjective = adjectives.AllAdjectives[randi()%(adjectives.AllAdjectives.size())].to_lower()
+	object = objects.AllObjects[randi()%(objects.AllObjects.size())].to_lower()
+	swapper = {"name": Name, "girl": sex, "she": she, "her": her, "age": ageStr, "adjective": adjective, "objects": object}
 	
 	var outroAIndex = randi()%(sentences.OutroSentences.size())
 	var outroA = sentences.OutroSentences[outroAIndex].format(swapper)
@@ -54,6 +57,11 @@ func _ready():
 	# Add a third sentence sometimes. Don't stutter.
 	var outroBIndex = randi()%(sentences.OutroSentences.size())
 	if outroBIndex > outroAIndex:
+		# redo randoms
+		adjective = adjectives.AllAdjectives[randi()%(adjectives.AllAdjectives.size())].to_lower()
+		object = objects.AllObjects[randi()%(objects.AllObjects.size())].to_lower()
+		swapper = {"name": Name, "girl": sex, "she": she, "her": her, "age": ageStr, "adjective": adjective, "objects": object}
+		
 		var outroB = sentences.OutroSentences[outroBIndex].format(swapper)
 		Bio = Bio + " " + _titleCase(outroB)
 	
