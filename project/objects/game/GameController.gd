4dabf1e3e33ba53d126f85e8ba687e4fbcef8abf
Devin Pentecost <devinpentecost@gmail.com>
2018-08-11 13:11:15 -0700

    Bowl better, also random every game

--- project/objects/game/GameController.gd
+++ project/objects/game/GameController.gd
@@ -27,6 +27,9 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	
+	#Randomize RNG
+	randomize()
+	
 	#Get all the kennels
 	_initialize_kennels()
 	
