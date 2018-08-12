58b5048d2e394f1dbe5ff2c0a18141d74bcffd36
Devin Pentecost <devinpentecost@gmail.com>
2018-08-12 16:12:02 -0700

    Slight change to toast pattern, toasts good to go

--- project/objects/ui/ResourceContainer.gd
+++ project/objects/ui/ResourceContainer.gd
@@ -11,8 +11,8 @@ export(Color) var danger_color
 export(Color) var out_color = Color(1, 0, 0, 1)
 
 #How many kennels left till we get worried
-const KENNEL_COUNT_WARNING = 7
-const KENNEL_COUNT_DANGER = 2
+const KENNEL_COUNT_WARNING = 10
+const KENNEL_COUNT_DANGER = 4
 
 #How much food to worry about
 const FOOD_COUNT_WARNING = 5
@@ -32,13 +32,21 @@ func update_food(current_food, max_food):
 	
 	#How much food is remaining?
 	var food_text = "%02d/%02d" % [current_food, max_food]
+	var toast = null
 	var text_color = safe_color
 	if current_food == 0:
 		text_color = out_color
+		toast = "You're out of food! Get some more!"
 	elif current_food < FOOD_COUNT_DANGER:
 		text_color = danger_color
+		toast = "Almost out of food!"
 	elif current_food < FOOD_COUNT_WARNING:
 		text_color = warning_color
+		toast = "Running low on food..."
+	
+	#Alert the player?
+	if toast:
+		get_tree().call_group("gui", "_emit_toast", toast)
 	
 	#Update the label
 	$FoodContainer/FoodAmount.text = food_text
@@ -50,13 +58,21 @@ func update_kennels(current_kennels, total_kennels):
 	
 	#How many kennels remaining?
 	var kennel_text = "%02d/%02d" % [current_kennels, total_kennels]
+	var toast = null
 	var text_color = safe_color
 	if current_kennels == 0:
 		text_color = out_color
+		toast = "Out of kennels! One more dog and it's over..."
 	elif current_kennels < KENNEL_COUNT_DANGER:
 		text_color = danger_color
+		toast = "Almost out of kennels, get to work!"
 	elif current_kennels < KENNEL_COUNT_WARNING:
 		text_color = warning_color
+		toast = "Running low on empty kennels..."
+	
+	#Alert the player?
+	if toast:
+		get_tree().call_group("gui", "_emit_toast", toast)
 	
 	#Update the label
 	$KennelContainer/KennelAmount.text = kennel_text
